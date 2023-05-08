9ddf1ad84be50449b9d747fd6f0eb76a7f4e23ec
Kedarinadh Sai Harsha Gadu <119718764+SaiHarsha9992@users.noreply.github.com>
2023-05-08 20:29:09 +0530

    Add files via upload

---
+++ Pass or Fail.py
@@ -0,0 +1,9 @@
+e = int(input())
+m = int(input())
+p = int(input())
+c = int(input())
+s = int(input())
+if ( e >= 35) and ( m >= 35) and ( p >= 35) and ( c >= 35) and ( s >= 35):
+    print("PASS")
+else:
+    print("FAIL")
