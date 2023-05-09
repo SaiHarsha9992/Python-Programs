b96f6463a9ed870c8b0f69e79d7136af290feaf5
Kedarinadh Sai Harsha Gadu <119718764+SaiHarsha9992@users.noreply.github.com>
2023-05-09 21:13:14 +0530

    Add files via upload

---
+++ Triangle.py
@@ -0,0 +1,9 @@
+a = int(input())
+b = int(input())
+c = int(input())
+if(a==b)and(b==c):
+        print("Equilateral triangle")
+elif (a==b)or(a==c)or(b==c):
+        print("Isosceles triangle")
+else:
+        print("Scalene triangle")
