59c82a0e519a92bb766e99b4b08b77b2cbef6647
Kedarinadh Sai Harsha Gadu <119718764+SaiHarsha9992@users.noreply.github.com>
2023-10-13 22:29:29 +0530

    Add files via upload

---
+++ Random.py
@@ -0,0 +1,7 @@
+import random
+
+movie_list = ['P. Venkata Rao', 'P. Appala Reddy', 'T. Appa Rao', 'K. Venkatesh', 'R. Appala Raju','M. Ramu','M. Rambabu','Y. Sanyasi Rao','Mylapalli Ramana''S. Srinivasa Rao','Y. Venkata Satyanarayana','K. Rama Krishna','K. Nooka Raju','S. Appala Naidu','Ch. Venkata Ramana','K.r. Krishna Vara Prasad','B. Eswara Rao','P. Satyanarayana','M. Venkata Ramana','B. Appala Raju','B. Appala Raju','L. Varahalu Babu','K. Demudu','K. Sathhi Babu','B. Venkata Ramana','M. Nageswara Rao','A.Venkata.Prakash','Guruvu setti','B. Suribabu','U.Bangar Raju','A.Shankar Rao','I.Prasad','M.Srinivasa Rao','S.Venkat','P. Venkata Ramana','B.Suresh','A.Sathish','Y.R. Govindh','S.Nooka Raju','N.Srinivasa Rao','N.Narayana Rao','Dhoni.Ramana','B.Dhanraj','D.Prasad','B.Srinu','M.Ramana','Y.Annavaram','U.Surya Rao','S.Mahesh','B.Radha','Y. Nookaratnam','R.Kumari','Y.Adi Lakshmi','G. Varalakshmi','Y. Kasi Lakshmi','P. Kumari','K. Nagaraju','N.Rajya Lakshmi','S. Santhosh Kumari','M. Suguna','P. Srinu','K. Dhana Raju','Y. Appala Raju','M.Pothu Raju']
+
+# pick a random element from a list of strings.
+movie = random.choice(movie_list)
+print(movie)
